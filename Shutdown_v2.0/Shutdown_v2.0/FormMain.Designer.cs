09648dc3f845f34e72f3df1f131b57419fb96bfc
hramelk <hramelk95@gmail.com>
2017-09-26 01:28:13 +0400

    Initial file upload

---
+++ Shutdown_v2.0/Shutdown_v2.0/FormMain.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace Shutdown_v2._0 {
+    partial class FormMain {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.radioButtonMinutes = new System.Windows.Forms.RadioButton();
+            this.radioButtonHours = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.time_number = new System.Windows.Forms.NumericUpDown();
+            this.buttonShutdown = new System.Windows.Forms.Button();
+            this.buttonRestart = new System.Windows.Forms.Button();
+            this.buttonAbort = new System.Windows.Forms.Button();
+            this.checkBoxForce = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonExit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.time_number)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radioButtonMinutes
+            // 
+            this.radioButtonMinutes.AutoSize = true;
+            this.radioButtonMinutes.Checked = true;
+            this.radioButtonMinutes.Location = new System.Drawing.Point(102, 18);
+            this.radioButtonMinutes.Name = "radioButtonMinutes";
+            this.radioButtonMinutes.Size = new System.Drawing.Size(61, 17);
+            this.radioButtonMinutes.TabIndex = 0;
+            this.radioButtonMinutes.TabStop = true;
+            this.radioButtonMinutes.Text = "minutes";
+            this.radioButtonMinutes.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonHours
+            // 
+            this.radioButtonHours.AutoSize = true;
+            this.radioButtonHours.Location = new System.Drawing.Point(102, 51);
+            this.radioButtonHours.Name = "radioButtonHours";
+            this.radioButtonHours.Size = new System.Drawing.Size(51, 17);
+            this.radioButtonHours.TabIndex = 1;
+            this.radioButtonHours.TabStop = true;
+            this.radioButtonHours.Text = "hours";
+            this.radioButtonHours.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "In";
+            // 
+            // time_number
+            // 
+            this.time_number.Location = new System.Drawing.Point(35, 33);
+            this.time_number.Maximum = new decimal(new int[] {
+            2147483647,
+            0,
+            0,
+            0});
+            this.time_number.Name = "time_number";
+            this.time_number.Size = new System.Drawing.Size(53, 20);
+            this.time_number.TabIndex = 0;
+            this.time_number.ValueChanged += new System.EventHandler(this.time_number_ValueChanged);
+            this.time_number.Enter += new System.EventHandler(this.time_number_Enter);
+            // 
+            // buttonShutdown
+            // 
+            this.buttonShutdown.Location = new System.Drawing.Point(278, 12);
+            this.buttonShutdown.Name = "buttonShutdown";
+            this.buttonShutdown.Size = new System.Drawing.Size(75, 23);
+            this.buttonShutdown.TabIndex = 4;
+            this.buttonShutdown.Text = "Shutdown";
+            this.buttonShutdown.UseVisualStyleBackColor = true;
+            this.buttonShutdown.Click += new System.EventHandler(this.buttonShutdown_Click);
+            // 
+            // buttonRestart
+            // 
+            this.buttonRestart.Location = new System.Drawing.Point(278, 51);
+            this.buttonRestart.Name = "buttonRestart";
+            this.buttonRestart.Size = new System.Drawing.Size(75, 23);
+            this.buttonRestart.TabIndex = 5;
+            this.buttonRestart.Text = "Restart";
+            this.buttonRestart.UseVisualStyleBackColor = true;
+            this.buttonRestart.Click += new System.EventHandler(this.buttonRestart_Click);
+            // 
+            // buttonAbort
+            // 
+            this.buttonAbort.Location = new System.Drawing.Point(16, 99);
+            this.buttonAbort.Name = "buttonAbort";
+            this.buttonAbort.Size = new System.Drawing.Size(75, 23);
+            this.buttonAbort.TabIndex = 6;
+            this.buttonAbort.Text = "Abort";
+            this.buttonAbort.UseVisualStyleBackColor = true;
+            this.buttonAbort.Click += new System.EventHandler(this.buttonAbort_Click);
+            // 
+            // checkBoxForce
+            // 
+            this.checkBoxForce.AutoSize = true;
+            this.checkBoxForce.Location = new System.Drawing.Point(224, 36);
+            this.checkBoxForce.Name = "checkBoxForce";
+            this.checkBoxForce.Size = new System.Drawing.Size(50, 17);
+            this.checkBoxForce.TabIndex = 3;
+            this.checkBoxForce.Text = "force";
+            this.checkBoxForce.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(167, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "perform a";
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonExit.Location = new System.Drawing.Point(170, 99);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(75, 23);
+            this.buttonExit.TabIndex = 7;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // FormMain
+            // 
+            this.AcceptButton = this.buttonShutdown;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.buttonExit;
+            this.ClientSize = new System.Drawing.Size(368, 141);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBoxForce);
+            this.Controls.Add(this.buttonAbort);
+            this.Controls.Add(this.buttonRestart);
+            this.Controls.Add(this.buttonShutdown);
+            this.Controls.Add(this.time_number);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButtonHours);
+            this.Controls.Add(this.radioButtonMinutes);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.Text = "Shutdown v2.0 by hramelk";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.time_number)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButtonMinutes;
+        private System.Windows.Forms.RadioButton radioButtonHours;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown time_number;
+        private System.Windows.Forms.Button buttonShutdown;
+        private System.Windows.Forms.Button buttonRestart;
+        private System.Windows.Forms.Button buttonAbort;
+        private System.Windows.Forms.CheckBox checkBoxForce;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonExit;
+    }
+}
+
---
+++ Shutdown_v2.0/Shutdown_v2.0/FormMain.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shutdown_v2._0 {
+    public partial class FormMain : Form {
+        public FormMain() {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e) {
+
+        }
+
+        private bool shutdown_command(string command_str) {
+            try {
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                startInfo.FileName = "shutdown.exe";
+                startInfo.Arguments = command_str;
+                process.StartInfo = startInfo;
+                process.Start();
+            } catch (Exception e) {
+                // process exception
+            }
+            return true;
+        }
+
+        private void time_number_Enter(object sender, EventArgs e) {
+            time_number.Select(0, time_number.Text.Length);
+        }
+
+        private void buttonShutdown_Click(object sender, EventArgs e) {
+
+            int time_value_int = Convert.ToInt32(time_number.Value);
+
+            if (time_value_int == 0) {
+                //time_number.BackColor = Color.Red;
+                time_number.BackColor = Color.Pink;
+                return;
+            } else {
+                time_number.BackColor = Color.White;
+            }
+
+            string arguments = "/s /t ";
+            if (checkBoxForce.Checked == true) {
+                arguments = "/s /f /t ";
+            }
+
+            if (radioButtonMinutes.Checked == true) {
+                time_value_int = time_value_int * 60;
+            } else if (radioButtonHours.Checked == true) {
+                time_value_int = time_value_int * 60 * 60;
+            }
+
+            string time_value_str = time_value_int.ToString();
+
+            shutdown_command(arguments + time_value_str);
+        }
+
+        private void buttonRestart_Click(object sender, EventArgs e) {
+            int time_value_int = Convert.ToInt32(time_number.Value);
+
+            if (time_value_int == 0) {
+                //time_number.BackColor = Color.Red;
+                time_number.BackColor = Color.Pink;
+                return;
+            } else {
+                time_number.BackColor = Color.White;
+            }
+
+            string arguments = "/r /t ";
+            if (checkBoxForce.Checked == true) {
+                arguments = "/r /f /t ";
+            }
+            
+            if (radioButtonMinutes.Checked == true) {
+                time_value_int = time_value_int * 60;
+            } else if (radioButtonHours.Checked == true) {
+                time_value_int = time_value_int * 60 * 60;
+            }
+            
+            string time_value_str = time_value_int.ToString();
+
+            shutdown_command(arguments + time_value_str);
+        }
+
+        private void buttonAbort_Click(object sender, EventArgs e) {
+            shutdown_command("/a");
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e) {
+            Application.Exit();
+        }
+
+        private void time_number_ValueChanged(object sender, EventArgs e) {
+            int time_value_int = Convert.ToInt32(time_number.Value);
+
+            if (time_value_int == 0) {
+                time_number.BackColor = Color.Pink;
+                return;
+            } else {
+                time_number.BackColor = Color.White;
+            }
+        }
+
+        //private void time_number_ValueChanged(object sender, EventArgs e) {
+        //    int time_value_int = Convert.ToInt32(time_number.Value);
+
+        //    if (time_value_int == 0) {
+        //        buttonShutdown.Enabled = false;
+        //        buttonRestart.Enabled = false;
+        //    } else {
+        //        buttonShutdown.Enabled = true;
+        //        buttonRestart.Enabled = true;
+        //    }
+        //}
+
+
+
+    }
+}
